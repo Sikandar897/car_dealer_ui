f682a9de7096952c693a590419de5bba024960b3
Sikandar897 <sikandarafridi6@gmail.com>
2024-09-30 19:00:45 +0500

    add: init

---
+++ lib/view/plan_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlanView extends StatelessWidget {
+  const PlanView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
